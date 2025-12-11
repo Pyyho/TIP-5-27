e2feeda615f637cdc5d5fc36c0035c4167a1aabb
Студент <student@win.mirea.ru>
2025-12-11 19:01:36 +0300

    readme

--- recipe-catalog/src/server.js
+++ recipe-catalog/src/server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000; // Используем порт 5000 вместо 3000
 
 app.listen(PORT, () => {
   console.log(`Сервер запущен на порту ${PORT}`);
